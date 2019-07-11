12914ad46456f1e73358bd156a00e94dc1d5a4c1
Jonathan Gouvea <jonathangouveasilva@gmail.com>
2019-07-11 18:26:45 -0300

    Banco de Dados 4.0

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 def initDB():
 	print(DATABASE_URL)
-	if DATABASE_URL == 'postgresql://localhost/labbd'
+	if DATABASE_URL == 'postgresql://localhost/labbd':
 		conn = psycopg2.connect(DATABASE_URL, user="postgres")
 	else
 		conn = psycopg2.connect(DATABASE_URL)
