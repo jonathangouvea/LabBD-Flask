9a9bf7affeb4a77a9596203b1de8a0ef160bf500
Jonathan Gouvea <jonathangouveasilva@gmail.com>
2019-07-11 17:42:29 -0300

    Banco de Dados inicial

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
-app = Flask(__name__)
+import db
 
+app = Flask(__name__)
+app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.postgres'),
+    )
 
 @app.route('/')
 def hello():
     return "Hello World!"
+    
+@app.route('/teste/')
+def hello():
+	db.initDB()
+    return "Hello DB!"   
 
 if __name__ == '__main__':
     app.run()
---
+++ db.py
@@ -0,0 +1,9 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+def initDB():
+	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+	with conn as cursor:
+    	cursor.execute(open("schema.sql", "r").read())
