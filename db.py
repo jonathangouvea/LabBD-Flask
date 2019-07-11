0a7027657fc1d960fbf820439eabd29907b8acce
Jonathan Gouvea <jonathangouveasilva@gmail.com>
2019-07-11 17:46:23 -0300

    Banco de Dados inicial 3.0

--- db.py
+++ db.py
@@ -6,4 +6,4 @@ DATABASE_URL = os.environ['DATABASE_URL']
 def initDB():
 	conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 	with conn as cursor:
-    	cursor.execute(open("schema.sql", "r").read())
+		cursor.execute(open("schema.sql", "r").read())
