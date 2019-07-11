be9c39b0a7a3c8ea8b837aa93b2dd6f162daaadd
Jonathan Gouvea <jonathangouveasilva@gmail.com>
2019-07-11 17:45:09 -0300

    Banco de Dados inicial 2.0

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ import db
 
 app = Flask(__name__)
 app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.postgres'),
-    )
+		SECRET_KEY='dev',
+		DATABASE=os.path.join(app.instance_path, 'flaskr.postgres'),
+	)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+	return "Hello World!"
     
 @app.route('/teste/')
 def hello():
 	db.initDB()
-    return "Hello DB!"   
+	return "Hello DB!"   
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
